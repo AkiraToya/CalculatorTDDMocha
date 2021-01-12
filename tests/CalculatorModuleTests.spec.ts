1eed5a87729a7a260791869841453c901dcaccdc
AkiraToya <akira_toya2006@yahoo.com>
2021-01-12 10:18:25 +0700

    Result 0 on calculate when no calculation string

--- tests/CalculatorModuleTests.spec.ts
+++ tests/CalculatorModuleTests.spec.ts
@@ -2,11 +2,20 @@ import { expect } from "chai"
 
 class CalculatorModule{
     result = 0
+
+    calculate = () => {}
 }
 
 describe("Test CalculatorModule behaviour", function(){
     it("test_init_doNothing", () => {
         let sut = new CalculatorModule()
+        expect(sut.result).to.be.equal(0)
+    })
+
+    it("test_calculate_noInput_returnZero", () => {
+        let sut = new CalculatorModule()
+        sut.calculate()
+
         expect(sut.result).to.be.equal(0)
     })
 })
